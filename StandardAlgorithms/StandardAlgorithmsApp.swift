9a35cf1e0016ffa2df54f6d6710fd7753be9736a
Rafe Marriott <Marriott.R@etoncollege.org.uk>
2024-11-05 10:53:48 +0000

    Initial Commit

---
+++ StandardAlgorithms/StandardAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandardAlgorithmsApp.swift
+//  StandardAlgorithms
+//
+//  Created by Marriott, Rafe (NA) on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StandardAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StandardAlgorithmsTests/StandardAlgorithmsTests.swift
@@ -0,0 +1,17 @@
+//
+//  StandardAlgorithmsTests.swift
+//  StandardAlgorithmsTests
+//
+//  Created by Marriott, Rafe (NA) on 05/11/2024.
+//
+
+import Testing
+@testable import StandardAlgorithms
+
+struct StandardAlgorithmsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

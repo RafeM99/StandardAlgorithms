da30c61b22e1e5826bdd07b972dfa20c7d0b2fda
Rafe Marriott <Marriott.R@etoncollege.org.uk>
2024-11-05 11:20:11 +0000

    bubble sort test

---
+++ StandardAlgorithms/Sorting.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  StandardAlgorithms
+//
+//  Created by Marriott, Rafe (NA) on 05/11/2024.
+//
+
+import Foundation
+
+class Sorting {
+    func bubbleSort(_ shuffledArray: [Int]) -> [Int] {
+        for
+        return [0]
+    }
+}
---
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -0,0 +1,33 @@
+//
+//  SortingTest.swift
+//  StandardAlgorithmsTests
+//
+//  Created by Marriott, Rafe (NA) on 05/11/2024.
+//
+
+import Testing
+@testable import StandardAlgorithms
+
+struct SortingTest {
+
+    @Test func bubbleSort() async throws {
+        //arrange
+        let sorting = Sorting()
+        let shuffledArray = [4,2,8,9,5]
+        //act
+        let result = sorting.bubbleSort(shuffledArray)
+        //assert
+        #expect(result == [2,4,5,8,9])
+    }
+    
+    @Test("Testing Parameterised bubble sort", arguments: [[4,2,8,9,5],[8,2,7,10,0]],[[2,4,5,8,9],[0,2,7,8,10]])
+    func bubbleSortWithVariousArrays(numbersToSort: [Int], expected: [Int]) async throws {
+        //arrange
+        let sorting = Sorting()
+        //act
+        let result = sorting.bubbleSort(numbersToSort)
+        //assert
+        #expect(result == expected)
+    }
+
+}

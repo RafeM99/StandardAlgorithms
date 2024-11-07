42e504027957e39fa9739ebe7f17370d54c214ef
Rafe Marriott <Marriott.R@etoncollege.org.uk>
2024-11-07 13:03:44 +0000

    srthja

---
+++ StandardAlgorithms/Searching.swift
@@ -0,0 +1,8 @@
+//
+//  Searching.swift
+//  StandardAlgorithms
+//
+//  Created by Marriott, Rafe (NA) on 07/11/2024.
+//
+
+import Foundation
---
+++ StandardAlgorithmsTests/SearchingTest.swift
@@ -0,0 +1,18 @@
+//
+//  Searching.swift
+//  StandardAlgorithmsTests
+//
+//  Created by Marriott, Rafe (NA) on 07/11/2024.
+//
+
+import Testing
+
+struct Searching {
+
+    @Test func binarySearch() async throws {
+        //arrange
+        //act
+        //assert
+    }
+
+}
